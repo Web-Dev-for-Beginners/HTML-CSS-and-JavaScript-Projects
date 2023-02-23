f0c820b3a449c9801c4087d173f26985c7c6fff8
Trisha-tech <1234trishasahu@gmail.com>
2023-02-24 03:52:46 +0530

    added text animation and video carousel

---
+++ Background Video/Readme.md
@@ -0,0 +1,39 @@
+
+# Background Video
+
+
+
+
+
+
+
+
+This project named **Background Video** is designed for the demonstration of how to add the dynamic background in the website by adding video in the background section of the webpage. It also displays the animation in text and a video carousel (where video will start playing when the user hovers over it).
+
+## 💥 Features
+
+- Displays infinite video playing in the background.
+
+- Displays animation in the text.
+
+- Displays a video carousel where the video started playing when the user hovers over any of the slides of the carousel.
+
+
+## 📌 Tech Stack
+
+[![linkedin](https://img.shields.io/badge/HTML5-E34F26?style=for-the-badge&logo=html5&logoColor=white)](https://www.linkedin.com/)
+[![portfolio](https://img.shields.io/badge/CSS3-1572B6?style=for-the-badge&logo=css3&logoColor=white)](https://katherineoelsner.com/)
+[![twitter](https://img.shields.io/badge/JavaScript-323330?style=for-the-badge&logo=javascript&logoColor=F7DF1E)](https://twitter.com/)
+
+
+
+## 📸 Screenshots Of The Project
+
+
+
+
+
+## ✨ Demo Video Of The Project
+
+
+
--- Background Video/background.js
+++ Background Video/background.js
@@ -1,23 +1,79 @@
-const btn=document.querySelector(".btn"); //Access the button element
-const video=document.querySelector(".background"); //Access the video element
-const fa=document.querySelector(".fa-solid"); //Access the icon element
 const preloader = document.querySelector(".preloader"); //Access the preloader div element
-
-btn.addEventListener("click",()=>{
-    if(btn.classList.contains("pause")){
-        btn.classList.remove("pause"); //Make the video play on clicking
-        video.play(); //Play the video
-        fa.classList.add("fa-pause");  //Add Pause Icon
-        fa.classList.remove("fa-play"); //Remove Play Icon
-    }else{
-        btn.classList.add("pause"); //Make the video pause on clicking
-        video.pause(); //Pause the video
-        fa.classList.remove("fa-pause"); //Remove Pause Icon
-        fa.classList.add("fa-play"); //Add Play Icon
-    }
-});
 
 //Load the Preloader
 window.addEventListener("load", () => {
     preloader.style.zIndex = "-999";
-  });
+});
+
+const carousel = document.querySelector('.carousel'), //Access the carousel box
+    firstImage = carousel.querySelectorAll('video')[0], //Access the first slide
+    arrowIcons = document.querySelectorAll('.wrapper i'); //Access the prev and next icons
+
+let isDragStart = false, prevPageX, prevScrollLeft;
+
+
+const showHideIcons = () => {
+    // showing and hiding prev/next icon according to carousel scroll left value
+    let scrollWidth = carousel.scrollWidth - carousel.clientWidth; //getting max scrollable width
+    arrowIcons[0].style.display = carousel.scrollLeft == 0 ? "none" : "block";
+    arrowIcons[1].style.display = carousel.scrollLeft == scrollWidth ? "none" : "block";
+}
+
+arrowIcons.forEach(icon => {
+    icon.addEventListener("click", () => {
+        let firstImgWidth = firstImage.clientWidth + 14; // getting first image width and adding 14 margin value
+        // If clicked icon is left, reduce width value from the carousel scroll left else add to it
+        carousel.scrollLeft += icon.id == "left" ? -firstImgWidth : firstImgWidth;
+        setTimeout(() => showHideIcons(), 60); //calling showHideIcons after 60ms
+    });
+});
+
+const dragStart = (e) => {
+    //updating global variables value on mouse down event
+    isDragStart = true;
+    prevPageX = e.pageX || e.touches[0].pageX;
+    prevScrollLeft = carousel.scrollLeft;
+}
+
+const dragging = (e) => {
+    //scrollling images/carousel to left according to mouse pointer
+    if (!isDragStart) return;
+    e.preventDefault();
+    carousel.classList.add("dragging");
+    let positionDiff = (e.pageX || e.touches[0].pageX) - prevPageX;
+    carousel.scrollLeft = prevScrollLeft - positionDiff;
+    showHideIcons();
+}
+
+const dragStop = () => {
+    isDragStart = false;
+    carousel.classList.remove("dragging");
+}
+
+carousel.addEventListener("mousedown", dragStart);
+carousel.addEventListener("touchstart", dragStart);
+
+carousel.addEventListener("mousemove", dragging);
+carousel.addEventListener("touchmove", dragging);
+
+carousel.addEventListener("mouseup", dragStop);
+carousel.addEventListener("touchend", dragStop);
+
+carousel.addEventListener("mouseleave", dragStop);
+
+
+//Video Plays on Hover and Stops otherwise
+videos = carousel.querySelectorAll("video");
+
+videos.forEach(video => {
+    video.addEventListener("mouseenter", () => {
+        video.play();
+    });
+});
+
+videos.forEach(video => {
+    video.addEventListener("mouseleave", () => {
+        video.pause();
+    });
+});
+
